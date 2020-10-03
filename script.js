8b5135d2044b78a4a388b93b56f684c962deeb17
null-archive <72272166+null-archive@users.noreply.github.com>
2020-10-03 06:19:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+let mainString = "Null Archive Explorer [Version 1.0 BETA]";
+let inputString = "";
+let underscore = true;
+document.getElementById("console").innerHTML = mainString;
+
+function clamp(x, a, b)
+{
+	return Math.max(a, Math.min(x, b));
+}
+
+function update()
+{
+	if (underscore == true)
+	{
+		document.getElementById("console").innerHTML = mainString + inputString + "_";
+	}
+	if (underscore != true)
+	{
+		document.getElementById("console").innerHTML = mainString + inputString;
+	}
+}
+
+function print(value)
+{
+	mainString += "<br>" + value;
+	update();
+}
+
+inputString = "";
+print("Currently is in a developing state");
+print("There is nothing here yet.");
+print("");
+print("home\\users\\main\\\> ");
+
+function userPress(e)
+{
+	if (e.charCode == clamp(e.charCode, 0, 127))
+	{
+		inputString += String.fromCharCode(e.charCode);
+		update();
+	}
+}
+
+function doSomething()
+{
+	print("");
+	print("There is nothing here yet.");
+	print("");
+	print("home\\users\\main\\\> ");
+}
+
+function userInput(e)
+{
+	if (e.keyCode == 8)
+	{
+		inputString = inputString.slice(0, -1);
+		update();
+	}
+	if (e.keyCode == 13)
+	{
+		mainString = mainString + inputString;
+		inputString = "";
+		doSomething();
+	}
+}
+
+let undescoreUpdate = setInterval(function() {
+   underscore = !underscore;
+   update();
+ }, 500);
+document.addEventListener("keypress", userPress);
+document.addEventListener("keydown", userInput);
+
