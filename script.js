9bd21aa979bfd5a1b73880b342bb871ebfa0dc2e
null-archive <72272166+null-archive@users.noreply.github.com>
2020-10-11 01:33:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,137 @@
 let mainString = "Null Archive Explorer [Version 1.0 BETA]";
 let inputString = "";
 let underscore = true;
+let inputState = false;
 document.getElementById("console").innerHTML = mainString;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function clamp(x, a, b)
 {
 	return Math.max(a, Math.min(x, b));
 }
 
+
+
+
+
+
 function update()
 {
-	if (underscore == true)
+	if (inputState == true && underscore == true)
 	{
-		document.getElementById("console").innerHTML = mainString + inputString + "_";
+		document.getElementById("console").innerHTML = mainString;
+		document.getElementById("UI").innerHTML = inputString + "_" + "            ";
 	}
-	if (underscore != true)
+	else
 	{
-		document.getElementById("console").innerHTML = mainString + inputString;
+		document.getElementById("console").innerHTML = mainString;
+		document.getElementById("UI").innerHTML = inputString + "            ";
 	}
 }
 
-function print(value)
+function println(value)
 {
 	mainString += "<br>" + value;
 	update();
 }
+function print(value)
+{
+	mainString += value;
+	update();
+}
+
+function turnOnInput()
+{
+	inputState = true;
+	update();
+}
+
+function turnOffInput()
+{
+	inputState = false;
+	update();
+}
+
+
 
 inputString = "";
-print("Currently is in a developing state");
-print("");
-print("There is nothing here yet.");
-print("");
-print("home\\users\\main\\\> ");
+println("Currently is in a developing state.");
+println("The explorer is not functioning yet.");
+println("");
+if ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) )
+{
+	println("Sorry, the explorer is not suitable for mobile devices.");
+	println("");
+}
+println("home\\users\\main\\\> ");
+
+
+
+
+
 
 function userPress(e)
 {
-	if (e.charCode == clamp(e.charCode, 0, 127))
+	if (e.charCode == clamp(e.charCode, 0, 127) && inputState == true)
 	{
-		inputString += String.fromCharCode(e.charCode);
+		if (String.fromCharCode(e.charCode) != "\r")
+		{
+			inputString += String.fromCharCode(e.charCode);
+		}
 		update();
 	}
 }
 
 function doSomething()
 {
-	print("");
-	print("There is nothing here yet.");
-	print("");
-	print("home\\users\\main\\\> ");
+	println("");
+	println("The explorer is not functioning yet.");
+	println("");
+	println("home\\users\\main\\\> ");
+	inputString = "";
 }
 
 function userInput(e)
 {
-	if (e.keyCode == 8)
+	if (e.keyCode == 8 && inputState == true)
 	{
 		inputString = inputString.slice(0, -1);
 		update();
 	}
-	if (e.keyCode == 13)
+	if (e.keyCode == 13 && inputState == true)
 	{
 		mainString = mainString + inputString;
 		inputString = "";
 		doSomething();
+		console.log(inputString);
 	}
 }
 
-let undescoreUpdate = setInterval(function() {
-   underscore = !underscore;
-   update();
- }, 500);
+//let undescoreUpdate = setInterval(function() { underscore = !underscore; update(); }, 500);
+
+document.addEventListener("click", (event) => 
+{
+	if (document.getElementById("UI") != event.target)
+	{
+		turnOffInput();
+	}
+	else
+	{
+		turnOnInput();
+	}
+});
 document.addEventListener("keypress", userPress);
 document.addEventListener("keydown", userInput);
 
